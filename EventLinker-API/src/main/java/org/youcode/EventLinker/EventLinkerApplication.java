00b707053bac79c55d521465ba51991d723c0a54
Joual27 <endy.panda@Outlook.com>
2025-01-14 14:01:38 +0100

    project set up

---
+++ EventLinker-API/src/main/java/org/youcode/EventLinker/EventLinkerApplication.java
@@ -0,0 +1,13 @@
+package org.youcode.EventLinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventLinkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventLinkerApplication.class, args);
+	}
+
+}
---
+++ EventLinker-API/src/test/java/org/youcode/EventLinker/EventLinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.youcode.EventLinker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventLinkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
