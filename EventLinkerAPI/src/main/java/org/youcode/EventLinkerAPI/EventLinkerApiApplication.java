400740bdfbf6608d5ce687265c5969dfec48cbd8
Joual27 <endy.panda@Outlook.com>
2025-01-14 14:36:25 +0100

    initial project setup for back-end

---
+++ EventLinkerAPI/src/main/java/org/youcode/EventLinkerAPI/EventLinkerApiApplication.java
@@ -0,0 +1,13 @@
+package org.youcode.EventLinkerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventLinkerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventLinkerApiApplication.class, args);
+	}
+
+}
---
+++ EventLinkerAPI/src/main/java/org/youcode/EventLinkerAPI/User/User.java
@@ -0,0 +1,8 @@
+package org.youcode.EventLinkerAPI.User;
+
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+}
