93120054f75b7869a65590bde46732cf0af10ab9
Joual27 <endy.panda@Outlook.com>
2025-03-03 21:29:55 +0000

    removed all unused imports

--- EventLinkerAPI/src/main/java/org/youcode/EventLinkerAPI/config/chat/WebSocketConfig.java
+++ EventLinkerAPI/src/main/java/org/youcode/EventLinkerAPI/config/chat/WebSocketConfig.java
@@ -1,27 +1,16 @@
 package org.youcode.EventLinkerAPI.config.chat;
 
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.http.server.ServletServerHttpRequest;
+
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.HandshakeInterceptor;
 import org.youcode.EventLinkerAPI.shared.utils.security.JwtService;
 import org.youcode.EventLinkerAPI.shared.utils.ws.StompChannelInterceptor;
-import org.youcode.EventLinkerAPI.user.User;
-import org.youcode.EventLinkerAPI.user.UserDAO;
-
-import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
